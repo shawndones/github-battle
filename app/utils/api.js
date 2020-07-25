02cf49e7e6e0433b3c28ab85c5260aa9eb780936
Shawn Dones <sdones@cj00686.local>
2020-07-25 16:15:45 -0400

    initializing git

---
+++ app/utils/api.js
@@ -0,0 +1,79 @@
+import { FaNapster } from "react-icons/fa"
+
+const id = "d67b60e6fb3c7faab6e8"
+const sec =  "788e282d7faf0a8468bfe714fd213e47d96f9468"
+const params = `?client_id=${id}&client_secret=${sec}&`
+
+function getErrorMsg(message, username) {
+  if(message === 'Not Found') {
+    return `${username} doesn't exist`
+  }
+  return message
+}
+
+function getProfile(username) {
+  // ?client_id=d67b60e6fb3c7faab6e8&client_secret=788e282d7faf0a8468bfe714fd213e47d96f9468
+  return fetch(`https://api.github.com/users/${username}${params}`)
+    .then((res)=> res.json())
+    .then((profile) => {
+      if (profile.message) {
+        throw new Error(getErrorMsg(profile.message, username))
+      }
+      return profile
+    })
+}
+
+function getRepos (username) {
+  return fetch(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
+    .then((res) => res.json())
+    .then((repos) => {
+      if (repos.message) {
+        throw new Error(getErrorMsg(repos.message, username))
+      }
+
+      return repos
+    })
+}
+
+function getStarCount (repos) {
+  return repos.reduce((count, { stargazers_count }) => count + stargazers_count , 0)
+}
+function calculateScore(followers, repos) {
+  return (followers *3) + getStarCount(repos)
+}
+
+function getUserData(player) {
+  return Promise.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(([ profile, repos ]) => ({
+    profile,
+    score: calculateScore(profile.followers, repos)
+  }))
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score)
+}
+
+export function battle(players) {
+  return Promise.all([
+    getUserData(players[0]),
+    getUserData(players[1])
+  ]).then((results)=> sortPlayers(results))
+}
+
+export function fetchPopularRepos(language) {
+  
+  const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
+
+  return fetch(endpoint)
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data.items) {
+        throw new Error(data.message)
+      }
+
+      return data.items
+    })
+}
